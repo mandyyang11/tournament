b13db55c05bca6f2e16c399397c3afd0929eda5d
mandyyang11 <mandyyang11@yahoo.com>
2017-04-23 13:11:45 -0700

    feat: added register player method
    this method will create new player entry with the given name

--- tournament.py
+++ tournament.py
@@ -45,7 +45,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-
+    conn, c = connect()
+    c.execute("INSERT INTO players (name) VALUES (%s);", (name,))
+    conn.commit()
+    conn.close()
+    
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
