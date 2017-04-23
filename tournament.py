8b58878a9c7970ca83017607bd8c860748a0aa64
mandyyang11 <mandyyang11@yahoo.com>
2017-04-23 13:13:39 -0700

    feat: added player standing method
    this method will return the list of players sorted with their place in
    the matches

--- tournament.py
+++ tournament.py
@@ -49,7 +49,7 @@ def registerPlayer(name):
     c.execute("INSERT INTO players (name) VALUES (%s);", (name,))
     conn.commit()
     conn.close()
-    
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -64,6 +64,40 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    # place all players in a dictionary
+    player_dict = {}
+    conn, c = connect()
+    c.execute("""SELECT * FROM players;""")
+    for row in c.fetchall():
+        player_dict[row[0]] = [row[1], 0, 0]
+
+    # count the number of win and matches in for all matches
+    c.execute("""SELECT winner, loser FROM matches;""")
+    for row in c.fetchall():
+        if row[0] in player_dict:
+            player_dict[row[0]][1] += 1
+            player_dict[row[0]][2] += 1
+        if row[1] in player_dict:
+            player_dict[row[1]][2] += 1
+
+    # compile win counts as the key to dictionary
+    win_count = {}
+    for i in player_dict:
+        wins = player_dict[i][1]
+        if wins in win_count:
+            win_count[wins].append((i, player_dict[i][0],
+                                    wins, player_dict[i][2]))
+        else:
+            win_count[wins] = [(i, player_dict[i][0],
+                                wins, player_dict[i][2])]
+
+    # compile output list
+    output_list = []
+    for i in sorted(win_count.keys(), reverse=True):
+        for j in win_count[i]:
+            output_list.append(j)
+
+    return output_list
 
 
 def reportMatch(winner, loser):
